6db431f9913fd8b9657def4a7b4077d8400c7e18
pokytester <fellipe.patrick678@gmail.com>
2022-10-14 16:22:37 -0300

    up

--- att.py
+++ att.py
@@ -1,10 +1,19 @@
+from urllib import response
 import requests
 api_url = "http://jsonplaceholder.typicode.com"
 
 def criar_t():
-    id1 = input(str('Digite o id que você quer alterar:'))
-    url = api_url +'/users/{}'+ "/todos".format(id1)
-    response = requests.get(url)
-    print(type(response))
-    
-print ('hello')
+    id = str(input('Digite o id que você quer alterar:'))
+    url = api_url +'/users/'+id+ "/todos"
+    responsecheck = requests.get(url)
+    response = requests.get(url).json()
+    if responsecheck.status_code== 200:
+        for task in response:
+            print('Título: ', task['title'])
+            print('ID', task['id'])
+            print('Status: ', task['completed'])
+            print()
+     
+       
+
+criar_t()
