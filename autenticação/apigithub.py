1a124d81db9e8abb9a552dc66efa8e94f44dce31
pokytester <fellipe.patrick678@gmail.com>
2022-11-04 17:54:57 -0300

    up

--- autenticação/apigithub.py
+++ autenticação/apigithub.py
@@ -4,11 +4,10 @@ from getpass import getpass
 
 user = "pokytester"
 password = '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'
- 
-p = str(input('deletar, ver meus, seguir:'))
- 
- 
 
+ 
+p = str(input('deletar, ver meus, seguir, checar:'))
+ 
 if p == 'deletar':
     response = requests.delete('https://api.github.com/user/following'+'/DUARTEzinho',
         auth = HTTPBasicAuth(user, password))
@@ -21,7 +20,9 @@ elif p == 'seguir':
     response = requests.put('https://api.github.com/user/following'+'/DUARTEzinho',
         auth = HTTPBasicAuth(user, password))
 
- 
+elif p == 'checar':
+    response = requests.get('https://api.github.com/users/dvcirilo/following/maikerosa',
+        auth = HTTPBasicAuth(user, password))
  
   
 print(response.text)
