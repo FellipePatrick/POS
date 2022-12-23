6a4b5f3744684a6ffb663732f838f50221de7e0d
Fellipe Patrick Lima de Brito <3254783@ifrn.local>
2022-12-23 17:31:17 -0300

    tudo

--- serviço/app.py
+++ serviço/app.py
@@ -14,11 +14,9 @@ def index():
 @app.route('/user/<int:user_id>')
 def users(user_id):
    if(user_id == 1):
-        return jsonify (
-        {
-            "nome": "Alice",
-            "email": "alice@outlook.com",
-            "id": "1"
-        }
-    )
+        return jsonify ({"nome": "Alice","email": "alice@outlook.com","id": "1"})
+   elif(user_id == 2):
+         return jsonify ({"nome": "Fernando","email": "fernando@outlook.com","id": "2"})
+
+   
 app.run()
